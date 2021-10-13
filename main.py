3eb75de03b9ea26c3ed55c7eda75bd3e5568d70a
Matias <mati.olea.thomsen@gmail.com>
2021-10-13 19:12:57 -0300

    tenemos layout

--- main.py
+++ main.py
@@ -3,9 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')          
 def hello_world():
-    return render_template('index.html')
-
-print('ueuasdas')
+    return render_template('algunawea.html')
 
 if __name__=="__main__":   
     app.run(debug=True)    
