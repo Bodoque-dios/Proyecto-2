e1b466cd585ea6f502f0a27d7530b2507472a646
Matias <mati.olea.thomsen@gmail.com>
2021-10-18 22:25:17 -0300

    pagina de inicio de sesion y registrarse 80%

--- main.py
+++ main.py
@@ -1,9 +1,23 @@
-from flask import Flask, render_template, request
-app = Flask(__name__)    
+from flask import Flask, render_template, request, redirect, session, flash, jsonify
+app = Flask(__name__)
 
-@app.route('/')          
+
+@app.route('/')
 def hello_world():
     return render_template('algunawea.html')
 
-if __name__=="__main__":   
-    app.run(debug=True)    
+@app.route('/revisar', methods=['POST'])
+def check():
+    pass
+
+
+@app.route('/signin.html')
+def signin():
+    return render_template('/signin.html')
+
+@app.route('/signup.html')
+def signup():
+    return render_template('/signup.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
